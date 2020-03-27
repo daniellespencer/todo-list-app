d89af9b07db4e76cd8c77457948975ea35fe59eb
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-03-27 18:20:17 -0500

    finished logic

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // check off the specific todos by clicking
-$("li").click(function(event){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
     
 });
 
 //click on x to delete todo
-$("span").click(function(){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//creating todos
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+
+    };
 });
